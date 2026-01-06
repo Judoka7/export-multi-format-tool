f8f8392af503acfebe2d1fece4c9207cdd16689c
Judoka7 <mmjoun44@gmail.com>
2026-01-06 16:01:52 +0100

    app.js

--- app.js
+++ app.js
@@ -1,20 +1,32 @@
 const upload = document.getElementById("upload");
-const igPost = document.getElementById("igPost");
-const igStory = document.getElementById("igStory");
-const twitter = document.getElementById("twitter");
 const exportBtn = document.getElementById("exportBtn");
+
+const canvases = {
+  igPost: document.getElementById("igPost"),
+  igStory: document.getElementById("igStory"),
+  twitter: document.getElementById("twitter"),
+  youtube: document.getElementById("youtube"),
+};
+
+const checkboxes = {
+  igPost: document.getElementById("cbIgPost"),
+  igStory: document.getElementById("cbIgStory"),
+  twitter: document.getElementById("cbTwitter"),
+  youtube: document.getElementById("cbYoutube"),
+};
 
 const sizes = {
   igPost: [1080, 1080],
   igStory: [1080, 1920],
   twitter: [1200, 675],
+  youtube: [1280, 720],
 };
 
 function drawImage(canvas, img, w, h) {
   canvas.width = w;
   canvas.height = h;
   const ctx = canvas.getContext("2d");
-  ctx.fillStyle = "#fff";
+  ctx.fillStyle = "#ffffff";
   ctx.fillRect(0, 0, w, h);
 
   const ratio = Math.min(w / img.width, h / img.height);
@@ -26,20 +38,26 @@ function drawImage(canvas, img, w, h) {
 
 upload.addEventListener("change", () => {
   const file = upload.files[0];
+  if (!file) return;
+
   const img = new Image();
   img.onload = () => {
-    drawImage(igPost, img, ...sizes.igPost);
-    drawImage(igStory, img, ...sizes.igStory);
-    drawImage(twitter, img, ...sizes.twitter);
+    for (let key in canvases) {
+      drawImage(canvases[key], img, ...sizes[key]);
+    }
   };
   img.src = URL.createObjectURL(file);
 });
 
 exportBtn.addEventListener("click", async () => {
   const zip = new JSZip();
-  zip.file("instagram.png", igPost.toDataURL().split(",")[1], { base64: true });
-  zip.file("story.png", igStory.toDataURL().split(",")[1], { base64: true });
-  zip.file("twitter.png", twitter.toDataURL().split(",")[1], { base64: true });
+
+  for (let key in canvases) {
+    if (checkboxes[key].checked) {
+      const canvas = canvases[key];
+      zip.file(`${key}.png`, canvas.toDataURL().split(",")[1], { base64: true });
+    }
+  }
 
   const content = await zip.generateAsync({ type: "blob" });
   const a = document.createElement("a");
